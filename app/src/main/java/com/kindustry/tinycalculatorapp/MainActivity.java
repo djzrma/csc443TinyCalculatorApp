b258216a1c0a9a1521fa673b9c796e50ea65c5c8
Donavan <jimenezdonavan@gmail.com>
2025-07-15 08:43:48 -0700

    Initial Commit App V 1.1
    UI developed, need to add behavior.

--- app/src/main/java/com/kindustry/tinycalculatorapp/MainActivity.java
+++ app/src/main/java/com/kindustry/tinycalculatorapp/MainActivity.java
@@ -1,6 +1,8 @@
 package com.kindustry.tinycalculatorapp;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,5 +22,55 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        additionButton();
+        subtractionButton();1
+        multiplicationButton();
+        divisionButton();
     }
+
+    public void additionButton(){
+        Button add = findViewById(R.id.additionSymbolButton);
+
+        add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+    }
+
+    public void subtractionButton(){
+        Button subtract = findViewById(R.id.subtractionSymbolButton);
+
+        subtract.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+    }
+
+    public void multiplicationButton(){
+        Button multiply = findViewById(R.id.multiplicationSymbolButton);
+
+        multiply.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+    }
+
+    public void divisionButton(){
+        Button divide = findViewById(R.id.divisionSymbolButton);
+
+        divide.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+    }
+
 }
